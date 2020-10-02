dd7c25edc64c049d37baa13dc3f5fc62a4c52806
Jimmy Chu <rjchu@uwaterloo.ca>
2020-10-02 00:05:51 -0400

    maybe this works now?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_002148) do
+ActiveRecord::Schema.define(version: 2020_10_02_040236) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
